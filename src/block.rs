1fd410fb112ef4783f993c0e1737f4d047eed6d2
Nick Hamann <nick@wabbo.org>
2015-04-20 23:59:47 -0500

    Add constructors for Block

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,35 @@
-use super::MultiHash;
+use super::{MultiHash, HashFnCode};
 
+// A singular block of data in IPFS
 pub struct Block {
     mh: MultiHash,
     data: Vec<u8>,
+}
+
+impl Block {
+    pub fn new() -> Block {
+        Block {
+            mh: MultiHash::new(),
+            data: Vec::new(),
+        }
+    }
+
+    pub fn data(data: Vec<u8>) -> Block {
+        Block {
+            mh: MultiHash::encode(data, HashFnCode::Sha1),
+            data: data,
+        }
+    }
+
+    // This function trusts that the MultiHash `mh` is
+    // correct for `data`
+    //
+    //  TODO: Have a debug mode where we go ahead and
+    //  encode `data` to verify that `mh` is correct
+    pub fn data_with_hash(data: Vec<u8>, mh: MultiHash) -> Block {
+        Block {
+            mh: mh,
+            data: data,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate rust_multihash;
 
 pub use rust_multihash::multihash::VecMultiHash as MultiHash;
-pub use rust_multihash::multihash::DecodedMultiHash;
+pub use rust_multihash::multihash::{DecodedMultiHash, HashFnCode};
 mod dag;
 mod block;
 mod blockstore;
